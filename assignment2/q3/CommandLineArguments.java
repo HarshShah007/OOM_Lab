56fa56df0426dd72c99701cd18b86a46ee6da14e
Harsh Shah <harsh.shah.jayesh@gmail.com>
2015-08-05 15:35:52 +0530

    Added question 3

---
+++ assignment2/q3/CommandLineArguments.java
@@ -0,0 +1,7 @@
+public class CommandLineArguments {
+
+	public static void main(String[] args) {
+		System.out.println("Name is " + args[1] + args[2]);
+		System.out.println("Roll No. is " + args[0]);
+	}
+}
---
+++ assignment2/q3/Signature.java
@@ -0,0 +1,9 @@
+public class Signature {
+
+	public static void main(String[] args) {
+		System.out.println("Harsh Shah");
+		System.out.println("iit2014071@iiita.ac.in");
+		System.out.println("facebook.com/harsh.shah.jayesh");
+	}
+}
+
